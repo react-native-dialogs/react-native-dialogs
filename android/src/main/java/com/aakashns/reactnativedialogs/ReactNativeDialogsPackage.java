2fb53a34b10b98084b90dfb6394e5c0088b37643
Aakash N S <aakashns1@gmail.com>
2015-11-01 03:20:53 +0530

    Single Choice example

---
+++ android/src/main/java/com/aakashns/reactnativedialogs/ReactNativeDialogsPackage.java
@@ -0,0 +1,39 @@
+package com.aakashns.reactnativedialogs;
+
+import android.support.v4.app.FragmentActivity;
+
+import com.aakashns.reactnativedialogs.modules.SimpleDialogModule;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReactNativeDialogsPackage implements ReactPackage {
+    FragmentActivity mActivity;
+
+    public ReactNativeDialogsPackage(FragmentActivity fragmentActivity) {
+        mActivity = fragmentActivity;
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+            ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new SimpleDialogModule(reactContext, mActivity));
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return new ArrayList<>();
+    }
+}
---
+++ android/src/main/java/com/aakashns/reactnativedialogs/Test.java
@@ -0,0 +1,7 @@
+package com.aakashns.reactnativedialogs;
+
+public class Test {
+  public static int add(int x, int y) {
+    return x + y;
+  }
+}
